44d266c21c309e0edda39ef651ba90dd26acf811
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-10-31 09:34:30 -0700

    create variables for user input and return value when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,27 @@ window.onload = function () {
   form.onsubmit = function (event) {
     // then we print values to the story area;
     // we're hardcoding these values for now
-    document.querySelector("span#person1a").innerText = "person 1";
-    document.querySelector("span#person1b").innerText = "person 1";
-    document.querySelector("span#person1c").innerText = "person 1";
-    document.querySelector("span#person2a").innerText = "person 2";
-    document.querySelector("span#person2b").innerText = "person 2";
-    document.querySelector("span#animal").innerText = "an animal";
-    document.querySelector("span#verb").innerText = "verb";
-    document.querySelector("span#noun").innerText = "noun";
-    document.querySelector("span#exclamation").innerText = "exclamation";
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput = document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
+
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
 
     // then we show the story by removing the class attribute
     document.querySelector("div#story").removeAttribute("class");
     event.preventDefault();
   };
 };
+
